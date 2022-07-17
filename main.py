824c838c9098b202a4b51ecf5c04e856db2f7ec2
PRAJVAL <prajvalmohan143@gmail.com>
2022-07-17 19:25:34 +0530

    First commit

---
+++ main.py
@@ -0,0 +1,15 @@
+import pymongo
+
+
+client = pymongo.MongoClient("mongodb+srv://Prajval:Prajval1998@cluster0.vzcbx6t.mongodb.net/?retryWrites=true&w=majority")
+db = client.test
+print(db)
+
+d = {
+    "name":"Prajval",
+    "email" : "prajvalmohan143@gmail.com",
+    "surname" : "Wachkavade"
+}
+db1 = client['mongotest']
+coll = db1['test']
+coll.insert_one(d)
